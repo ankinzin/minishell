7e635c7126c5c2ecdb52e967cf935043d03f0ccb
Arafa Nkinzingabo <ankinzin@c1s4d15.42wolfsburg.de>
2023-05-26 11:31:25 +0200

    Start

---
+++ Makefile
@@ -0,0 +1,20 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ankinzin <ankinzin@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/05/23 12:47:13 by ankinzin          #+#    #+#              #
+#    Updated: 2023/05/23 12:47:17 by ankinzin         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Program name:
+NAME	= minishell
+
+# Compiler and flags
+CC		= cc
+CFLAGS	= -Wall -Wextra -Werror -g
+EFLAGS	= -lreadline
+RM		= rm -rf
---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ankinzin <ankinzin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 12:46:12 by ankinzin          #+#    #+#             */
+/*   Updated: 2023/05/23 12:46:14 by ankinzin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <signal.h>
